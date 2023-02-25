9811136f4d95cc502c57c5ed79d76fc648a4a052
swetzel <seb.o.wetzel@gmail.com>
2023-02-25 16:33:57 -0500

    Keymap config/cleanup

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -6,3 +6,5 @@ vim.keymap.set("n", "<C-f>", "<C-f>zz")
 vim.keymap.set("n", "<C-b>", "<C-b>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<leader>w", "<C-w>")
