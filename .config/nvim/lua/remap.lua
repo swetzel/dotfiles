4aefc4accfb051892b3f3d2317cb3cc48c1eb946
swetzel <seb.o.wetzel@gmail.com>
2023-02-25 04:17:40 -0500

    New keymaps

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -1,1 +1,8 @@
 vim.g.mapleader = " "
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-f>", "<C-f>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
