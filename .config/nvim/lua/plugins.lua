192eaeab28233115a981aaabeb5cb5581b064633
swetzel <seb.o.wetzel@gmail.com>
2023-02-25 03:31:02 -0500

    Initial commit

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,39 @@
+vim.cmd.packadd("packer.nvim")
+
+return require("packer").startup(function(use)
+
+    use({"wbthomason/packer.nvim"})
+    use({"olimorris/onedarkpro.nvim"})
+
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate"
+    })
+
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.1",
+        requires = {
+            {"nvim-lua/plenary.nvim"},
+        }
+    })
+
+    use({
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v1.x",
+        requires = {
+            {"neovim/nvim-lspconfig"},
+            {"williamboman/mason.nvim"},
+            {"williamboman/mason-lspconfig.nvim"},
+            {"hrsh7th/nvim-cmp"},
+            {"hrsh7th/cmp-nvim-lsp"},
+            {"hrsh7th/cmp-buffer"},
+            {"hrsh7th/cmp-path"},
+            {"saadparwaiz1/cmp_luasnip"},
+            {"hrsh7th/cmp-nvim-lua"},
+            {"L3MON4D3/LuaSnip"},
+            {"rafamadriz/friendly-snippets"},
+        }
+    })
+
+end)
---
+++ .vimrc
@@ -0,0 +1,7 @@
+syntax enable
+
+set hlsearch
+set showmatch
+set tabstop=4
+set encoding=utf8
+set expandtab
